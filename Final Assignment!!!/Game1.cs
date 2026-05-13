4000d7fee053d96d83ae911cebce4064aed754f0
Christian MACDONALD <362572158@GoTVDSB.ca>
2026-05-13 13:20:05 -0400

    sg

--- Final Assignment!!!/Game1.cs
+++ Final Assignment!!!/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Pokemon;
 
 namespace Final_Assignment___
 {
@@ -8,6 +9,13 @@ namespace Final_Assignment___
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        KeyboardState currentState, oldState;
+        SpriteFont menuFont, healthFont;
+        Rectangle window, menuLocation, moveInfoLocation, battleLocation, arrowSize;
+        Snorlax snorlax;
+        Texture2D snorlaxTexture, menu, healthbar, healthIcon, battleImg, arrow;
+        Vector2 moveType, moveName1, moveName2, moveName3, moveName4, typeText, PPText, movePP;
+
 
         public Game1()
         {
@@ -19,14 +27,37 @@ namespace Final_Assignment___
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
             base.Initialize();
+            window = new Rectangle(0, 0, 1000, 800);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+            menuLocation = new Rectangle(0, 550, 600, 250);
+            moveInfoLocation = new Rectangle(600, 550, 400, 250);
+            battleLocation = new Rectangle(0, 0, 1000, 550);
+            typeText = new Vector2(660, 600);
+            moveName1 = new Vector2(70, 600);
+            moveName2 = new Vector2(70, 680);
+            moveName3 = new Vector2(340, 600);
+            moveName4 = new Vector2(340, 680);
+            moveType = new Vector2(780, 600);
+            PPText = new Vector2(660, 670);
+            movePP = new Vector2(740, 670);
+            arrowSize = new Rectangle(20, 600, 50, 60);
+            snorlax = new Snorlax(snorlaxTexture, new Rectangle(150, 283, 300, 300));
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            snorlaxTexture = Content.Load<Texture2D>("snorlax");
+            healthbar = Content.Load<Texture2D>("tile");
+            healthIcon = Content.Load<Texture2D>("healthBar");
+            menu = Content.Load<Texture2D>("starterMoveset");
+            menuFont = Content.Load<SpriteFont>("pokeFont");
+            healthFont = Content.Load<SpriteFont>("healthFont");
+            battleImg = Content.Load<Texture2D>("grassBattlefield");
+            arrow = Content.Load<Texture2D>("select");
             // TODO: use this.Content to load your game content here
         }
 
@@ -36,15 +67,74 @@ namespace Final_Assignment___
                 Exit();
 
             // TODO: Add your update logic here
-
+            currentState = Keyboard.GetState();
+            if (arrowSize.X == 20 && arrowSize.Y == 600 && currentState.IsKeyDown(Keys.Down))
+                arrowSize.Y = 680;
+            else if (arrowSize.X == 20 && arrowSize.Y == 600 && currentState.IsKeyDown(Keys.Right))
+                arrowSize.X = 290;
+            else if (arrowSize.X == 20 && arrowSize.Y == 680 && currentState.IsKeyDown(Keys.Up))
+                arrowSize.Y = 600;
+            else if (arrowSize.X == 20 && arrowSize.Y == 680 && currentState.IsKeyDown(Keys.Right))
+                arrowSize.X = 290;
+            else if (arrowSize.X == 290 && arrowSize.Y == 600 && currentState.IsKeyDown(Keys.Down))
+                arrowSize.Y = 680;
+            else if (arrowSize.X == 290 && arrowSize.Y == 600 && currentState.IsKeyDown(Keys.Left))
+                arrowSize.X = 20;
+            else if (arrowSize.X == 290 && arrowSize.Y == 680 && currentState.IsKeyDown(Keys.Up))
+                arrowSize.Y = 600;
+            else if (arrowSize.X == 290 && arrowSize.Y == 680 && currentState.IsKeyDown(Keys.Left))
+                arrowSize.X = 20;
+            if (arrowSize.X == 20 && arrowSize.Y == 600)
+            {
+                if (currentState.IsKeyDown(Keys.A) && oldState.IsKeyUp(Keys.A))
+                {
+                    snorlax.Headbutt = true;
+                }
+            }    
+            snorlax.Update(gameTime);
+            oldState = currentState;
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(battleImg, battleLocation, Color.White);
+            _spriteBatch.Draw(menu, menuLocation, Color.White);
+            _spriteBatch.Draw(menu, moveInfoLocation, Color.White);
+            _spriteBatch.DrawString(menuFont, snorlax.Move1, moveName1, Color.Black);
+            _spriteBatch.DrawString(menuFont, snorlax.Move2, moveName2, Color.Black);
+            _spriteBatch.DrawString(menuFont, snorlax.Move3, moveName3, Color.Black);
+            _spriteBatch.DrawString(menuFont, snorlax.Move4, moveName4, Color.Black);
+            _spriteBatch.DrawString(menuFont, "Type/", typeText, Color.Black);
+            _spriteBatch.DrawString(menuFont, snorlax.MoveType, moveType, Color.Black);
+            if (arrowSize.X == 20 && arrowSize.Y == 600)
+            {
+                _spriteBatch.DrawString(menuFont, "PP         /15", PPText, Color.Black);
+                _spriteBatch.DrawString(menuFont, snorlax.Move1PP, movePP, Color.Black);
+            }
+            if (arrowSize.X == 20 && arrowSize.Y == 680)
+            {
+                _spriteBatch.DrawString(menuFont, "PP         /10", PPText, Color.Black);
+                _spriteBatch.DrawString(menuFont, snorlax.Move2PP, movePP, Color.Black);
+            }
+            if (arrowSize.X == 290 && arrowSize.Y == 600)
+            {
+                _spriteBatch.DrawString(menuFont, "PP         /5", PPText, Color.Black);
+                _spriteBatch.DrawString(menuFont, snorlax.Move3PP, movePP, Color.Black);
+            }
+            if (arrowSize.X == 290 && arrowSize.Y == 680)
+            {
+                _spriteBatch.DrawString(menuFont, "PP         /30", PPText, Color.Black);
+                _spriteBatch.DrawString(menuFont, snorlax.Move4PP, movePP, Color.Black);
+            }
+            _spriteBatch.Draw(arrow, arrowSize, Color.Black);
+            snorlax.Draw(_spriteBatch);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Final Assignment!!!/Pokemon/Snorlax.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using NVorbis.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    class Snorlax
+    {
+        private Texture2D _texture;
+        private Rectangle _location;
+        private bool _using_Headbutt, _using_Body_Press, _using_Hyper_Beam, _using_Defense_Curl;
+        private string _move1, _move2, _move3, _move4, _move_type, _move1_PP, _move2_PP, _move3_PP, _move4_PP;
+        private int _speed, _health, _defense, _attack, _sDefense;
+        private float _battle_time, _frame_time;
+        public Snorlax(Texture2D texture, Rectangle location)
+        {
+            _texture = texture;
+            _location = location;
+            _move1 = "Headbutt";
+            _move2 = "Body Press";
+            _move3 = "Hyper Beam";
+            _move4 = "Defense Curl";
+            _move_type = "Normal";
+            _move1_PP = "15";
+            _move2_PP = "10";
+            _move3_PP = "5";
+            _move4_PP = "30";
+            Random stats = new Random();
+            _speed = stats.Next(20, 41);
+            _health = stats.Next(150, 171);
+            _defense = stats.Next(50, 81);
+            _attack = stats.Next(100, 121);
+            _sDefense = stats.Next(100, 121);
+            _using_Headbutt = false;
+            _using_Body_Press = false;
+            _using_Defense_Curl = false;
+            _using_Hyper_Beam = false;
+        }
+
+        public void Update(GameTime gametime)
+        {
+            if (_using_Headbutt == true)
+            {
+                _battle_time += (float)gametime.ElapsedGameTime.TotalSeconds;
+                _frame_time += (float)gametime.ElapsedGameTime.TotalSeconds;
+                if (_frame_time <= 0.5f)
+                {
+                    _location.X += 15;
+                    _location.Y -= 10;
+                }
+                else if (_frame_time <= 1f)
+                {
+                    _location.X -= 15;
+                    _location.Y += 10;
+                }
+                else if (_battle_time >= 3f)
+                {
+                    _location.X = 150;
+                    _location.Y = 283;
+                    _battle_time = 0;
+                    _frame_time = 0;
+                    _using_Headbutt = false;
+                }
+            }
+            if (_using_Body_Press == true)
+            {
+                _battle_time += (float)gametime.ElapsedGameTime.TotalSeconds;
+                _frame_time += (float)gametime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+        public bool Headbutt
+        {
+            get { return _using_Headbutt; }
+            set { _using_Headbutt = value; }
+        }
+        public int Health
+        {
+            get { return _health; }
+        }
+        public string Move1
+        {
+            get { return _move1; }
+        }
+        public string Move2
+        {
+            get { return _move2; }
+        }
+        public string Move3
+        {
+            get { return _move3; }
+        }
+        public string Move4
+        {
+            get { return _move4; }
+        }
+        public string MoveType
+        {
+            get { return _move_type; }
+        }
+        public string Move1PP
+        {
+            get { return _move1_PP; }
+        }
+        public string Move2PP
+        {
+            get { return _move2_PP; }
+        }
+        public string Move3PP
+        {
+            get { return _move3_PP; }
+        }
+        public string Move4PP
+        {
+            get { return _move4_PP; }
+        }
+
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, Color.White);
+        }
+
+    }
+}
